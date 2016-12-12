a8e4ffefe00c1761ddc3904a4dd6bfcd6e356fed
chutipon29301 <chutiponh@hotmail.com>
2016-12-12 09:50:19 +0700

    Add button to show file name according to the file

--- src/Menu.java
+++ src/Menu.java
@@ -24,6 +24,7 @@ import java.awt.Font;
 import java.io.File;
 import java.util.ArrayList;
 
+import javax.print.DocFlavor.STRING;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 
@@ -58,6 +59,7 @@ public class Menu extends JFrame {
 		}else if (id.charAt(id.length() - 1) == '2') {
 			subject = "Physics";
 		}
+		
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 1003, 589);
 		contentPane = new JPanel();
@@ -77,31 +79,45 @@ public class Menu extends JFrame {
 		lblSubject.setBounds(175, 31, 109, 45);
 		contentPane.add(lblSubject);
 		
-		JButton btnVdo_1 = new JButton("VDO1");
+		String[] VDOName = getVDOName(id);
+		
+		JButton btnVdo_1 = new JButton((VDOName[0] == null) ? "No Video" : VDOName[0]);
 		btnVdo_1.setBounds(84, 162, 159, 93);
 		contentPane.add(btnVdo_1);
 		
-		JButton btnVdo_2 = new JButton("VDO2");
+		JButton btnVdo_2 = new JButton((VDOName[1] == null) ? "No Video" : VDOName[1]);
 		btnVdo_2.setBounds(417, 162, 159, 93);
 		contentPane.add(btnVdo_2);
 		
-		JButton btnVdo_3 = new JButton("VDO3");
+		JButton btnVdo_3 = new JButton((VDOName[2] == null) ? "No Video" : VDOName[2]);
 		btnVdo_3.setBounds(758, 162, 159, 93);
 		contentPane.add(btnVdo_3);
 		
-		JButton btnVdo_4 = new JButton("VDO4");
+		JButton btnVdo_4 = new JButton((VDOName[3] == null) ? "No Video" : VDOName[3]);
 		btnVdo_4.setBounds(84, 360, 159, 93);
 		contentPane.add(btnVdo_4);
 		
-		JButton btnVdo_5 = new JButton("VDO5");
+		JButton btnVdo_5 = new JButton((VDOName[4] == null) ? "No Video" : VDOName[4]);
 		btnVdo_5.setBounds(417, 360, 159, 93);
 		contentPane.add(btnVdo_5);
 		
-		JButton btnVdo_6 = new JButton("VDO6");
+		JButton btnVdo_6 = new JButton((VDOName[5] == null) ? "No Video" : VDOName[5]);
 		btnVdo_6.setBounds(758, 360, 159, 93);
 		contentPane.add(btnVdo_6);
 	}
 	
+	
+	public String[] getVDOName(String id) {
+		String[] fileName = new String[6];
+		ArrayList<String> fileNameList = getFileNameInFolder(id);
+		for (int i = 0; i < fileName.length; i++) {
+			try {
+				fileName[i] = fileNameList.get(i);
+			} catch (Exception e) {
+			}
+		}
+		return fileName;
+	}
 	public ArrayList<String> getFileNameInFolder(String id) {
 		String subject = (id.charAt(id.length() - 1) == '1') ? "Math" : "Physics";
 		File folder = new File("\\\\" + Index.DB_LOCATION + "\\vdo\\" + id.substring(0, id.length() - 1) + "\\" + subject);
